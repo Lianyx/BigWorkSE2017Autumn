d0224eea4f8a9c7859d3c757eff2348ff9507bd5
161250070 <161250070@mail.smal.nju.edu.cn>
2017-11-08 13:06:26 +0800

    Update sequence diagrams

---
+++ Client/src/main/java/dataService/memberdataService/MemberdataService.java
@@ -0,0 +1,22 @@
+package dataService.memberdataService;
+
+import po.MemberPO;
+import util.ResultMessage;
+import vo.MemberSearchVO;
+
+import java.util.ArrayList;
+
+
+public interface MemberdataService {
+    public ResultMessage insert(MemberPO MemberPO);
+    public ResultMessage delete(int id);
+    public ResultMessage delete(ArrayList<Integer> list);
+    public ResultMessage update(MemberPO MemberPO);
+    public ArrayList<MemberPO> select(MemberSearchVO memberSearchVO);
+    public ArrayList<MemberPO> select(String keyword);
+    public MemberPO showDetail(int id);
+    public ArrayList<MemberPO> getAll();
+
+
+
+}
