6d5f799ff4643fcd1749a7f8c9e824e8129ac434
161250072 <161250072@mail.smal.nju.edu.cn>
2017-12-15 19:17:00 +0530

    Delete InventoryGiftBiiPOMapper.java

--- Server/src/main/java/data/inventorydata/InventoryGiftBiiPOMapper.java
+++
@@ -1,7 +0,0 @@
-package data.inventorydata;
-
-import data.checkdata.ReceiptPOMapper;
-import po.InventoryGiftBillPO;
-
-public interface InventoryGiftBiiPOMapper extends ReceiptPOMapper<InventoryGiftBillPO> {
-}
