05dfbbb0e7c645b6bb5f030a74b9cf9b5129d608
161250072 <161250072@mail.smal.nju.edu.cn>
2017-12-15 19:20:12 +0530

    Delete InventoryWarningBillPOMapper.java

--- Server/src/main/java/data/inventorydata/InventoryWarningBillPOMapper.java
+++
@@ -1,7 +0,0 @@
-package data.inventorydata;
-
-import data.checkdata.ReceiptPOMapper;
-import po.InventoryWarningBillPO;
-
-public interface InventoryWarningBillPOMapper extends ReceiptPOMapper<InventoryWarningBillPO> {
-}
