733a1b0e62522a99a60103c9a349b7e7548711af
161250072 <161250072@mail.smal.nju.edu.cn>
2017-12-15 19:18:25 +0530

    Delete InventoryOverflowBillPOMapper.java

--- Server/src/main/java/data/inventorydata/InventoryOverflowBillPOMapper.java
+++
@@ -1,7 +0,0 @@
-package data.inventorydata;
-
-import data.checkdata.ReceiptPOMapper;
-import po.InventoryOverflowBillPO;
-
-public interface InventoryOverflowBillPOMapper extends ReceiptPOMapper<InventoryOverflowBillPO> {
-}
