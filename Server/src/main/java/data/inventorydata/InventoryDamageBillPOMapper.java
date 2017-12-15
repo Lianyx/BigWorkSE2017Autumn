60dcc1b8fb3eaf0c0cb48c3e7a7e16ebdbc3a460
161250072 <161250072@mail.smal.nju.edu.cn>
2017-12-15 19:16:40 +0530

    Delete InventoryDamageBillPOMapper.java

--- Server/src/main/java/data/inventorydata/InventoryDamageBillPOMapper.java
+++
@@ -1,7 +0,0 @@
-package data.inventorydata;
-
-import data.checkdata.ReceiptPOMapper;
-import po.InventoryDamageBillPO;
-
-public interface InventoryDamageBillPOMapper extends ReceiptPOMapper<InventoryDamageBillPO> {
-}
