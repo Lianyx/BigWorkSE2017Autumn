f4b7ecd37c83b584d0ea738cbc6173ebc095638b
林鹏 <161250069@smail.nju.edu.cn>
2018-01-08 13:54:49 +0800

    modify

--- Client/src/main/java/blService/accountblService/AccountblService.java
+++ Client/src/main/java/blService/accountblService/AccountblService.java
@@ -8,15 +8,7 @@ import java.util.List;
 import java.util.Set;
 
 public interface AccountblService {
-    //public List<AccountListVO> showAllAccounts()throws RemoteException;
 
-    //public List<AccountListVO> search(String keyword)throws RemoteException;
-
-    //public ResultMessage add(AccountListVO vo)throws RemoteException;
-
-    //public ResultMessage delete(int ID)throws RemoteException;
-
-    //public ResultMessage update(AccountVO accountVO)throws RemoteException;
 
     public ResultMessage add(AccountListVO accountListVO)throws RemoteException;
     public ResultMessage delete(int id)throws RemoteException;
