493782df9008756857e68edfe7a7efc18213106e
tiberius <161250065@smail.nju.edu.cn>
2018-01-13 15:45:04 +0800

    …………

--- Client/src/main/java/businesslogic/promotionbl/testprmt/TestSalesSell.java
+++ Client/src/main/java/businesslogic/promotionbl/testprmt/TestSalesSell.java
@@ -1,10 +1,17 @@
 package businesslogic.promotionbl.testprmt;
 
+import blService.promotionblService.CombinePromotionblService;
+import blService.promotionblService.PromotionInfo;
+import blService.promotionblService.PromotionblService;
 import blService.salesblService.SalesSellblService;
+import businesslogic.checkbl.MyServiceFactory;
+import businesslogic.promotionbl.MyblServiceFactory;
 import businesslogic.salesbl.SalesSellbl;
 import util.ReceiptState;
 import util.RespectiveReceiptSearchCondition;
 import vo.ListGoodsItemVO;
+import vo.promotionVO.CombinePromotionVO;
+import vo.promotionVO.PromotionVO;
 import vo.receiptVO.SalesSellReceiptVO;
 
 import java.net.MalformedURLException;
@@ -17,32 +24,32 @@ public class TestSalesSell {
 
     public static void main(String[] args) {
         ArrayList<ListGoodsItemVO> list = new ArrayList<>();
-        list.add(new ListGoodsItemVO("sabi", "12", "SABI", 100, 2, "nishisabi"));
-        list.add(new ListGoodsItemVO("sabi", "4", "SABI", 50, 2, "nishisabi"));
+        ListGoodsItemVO r1 = new ListGoodsItemVO("花灯2", "122", "小灯1", 54, 2, "nishisabi");
+        ListGoodsItemVO r2 = new ListGoodsItemVO("花灯2", "122", "小灯1", 54, 2, "nishisabi");
+        list.add(r1);
+        list.add(r2);
+//        list.add(new ListGoodsItemVO("sabi", "4", "SABI", 50, 2, "nishisabi"));
 
-        SalesSellReceiptVO salesSellReceiptVO = new SalesSellReceiptVO("JHT-20170101-00023", 12, LocalDateTime.now(), LocalDateTime.now().plusDays(1), ReceiptState.PENDING, -1, "linyuchao", "awb", "awd", list, "awd", 1200000, 12, 12, null, 123);
-        SalesSellReceiptVO salesSellReceiptVO1 = new SalesSellReceiptVO("JHT-20170101-00011", 12, LocalDateTime.now(), LocalDateTime.now().minusDays(4), ReceiptState.PENDING, 1, "linyuchao", "awx", "21c", list, "x", 12, 132, 12, null, 123);
-        SalesSellReceiptVO salesSellReceiptVO2 = new SalesSellReceiptVO("JHT-20170101-00000", 12, LocalDateTime.now(), LocalDateTime.now().plusDays(1), ReceiptState.PENDING, -1, "linyuchao", "brb", "cva", list, "cc", 12, 12123, 12, null, 123);
-        SalesSellReceiptVO salesSellReceiptVO3 = new SalesSellReceiptVO("JHT-20170101-00000", 12, LocalDateTime.now(), LocalDateTime.now().plusDays(1), ReceiptState.PENDING, 1, "linyuchao", "wad", "wadw", list, "beba", 12, 123, 12, null, 123);
-        SalesSellReceiptVO salesSellReceiptVO4 = new SalesSellReceiptVO("JHT-20170101-12333", 12, LocalDateTime.now(), LocalDateTime.now().plusDays(1), ReceiptState.PENDING, -1, "linyuchao", "awdaw", "awd", list, "qe1ewdawdwadjawidhzkhvkdhlwaclwawdawdawdhvilahwl", 12, 12, 12, null, 123);
-        SalesSellReceiptVO salesSellReceiptVO5 = new SalesSellReceiptVO("JHT-20170101-11515", 12, LocalDateTime.now(), LocalDateTime.now().plusDays(1), ReceiptState.PENDING, -1, "linyuchao", "", "", list, "2", 12, 12, 1882, null, 112223);
+//        SalesSellReceiptVO salesSellReceiptVO = new SalesSellReceiptVO("JHT-20170101-00023", 12, LocalDateTime.now(), LocalDateTime.now().plusDays(1), ReceiptState.PENDING, -1, "linyuchao", "awb", "awd", list, "awd", 1200000, 12, 12, null, 123);
+//        SalesSellReceiptVO salesSellReceiptVO1 = new SalesSellReceiptVO("JHT-20170101-00011", 12, LocalDateTime.now(), LocalDateTime.now().minusDays(4), ReceiptState.PENDING, 1, "linyuchao", "awx", "21c", list, "x", 12, 132, 12, null, 123);
+//        SalesSellReceiptVO salesSellReceiptVO2 = new SalesSellReceiptVO("JHT-20170101-00000", 12, LocalDateTime.now(), LocalDateTime.now().plusDays(1), ReceiptState.PENDING, -1, "linyuchao", "brb", "cva", list, "cc", 12, 12123, 12, null, 123);
+//        SalesSellReceiptVO salesSellReceiptVO3 = new SalesSellReceiptVO("JHT-20170101-00000", 12, LocalDateTime.now(), LocalDateTime.now().plusDays(1), ReceiptState.PENDING, 1, "linyuchao", "wad", "wadw", list, "beba", 12, 123, 12, null, 123);
+//        SalesSellReceiptVO salesSellReceiptVO4 = new SalesSellReceiptVO("JHT-20170101-12333", 12, LocalDateTime.now(), LocalDateTime.now().plusDays(1), ReceiptState.PENDING, -1, "linyuchao", "awdaw", "awd", list, "qe1ewdawdwadjawidhzkhvkdhlwaclwawdawdawdhvilahwl", 12, 12, 12, null, 123);
+//        SalesSellReceiptVO salesSellReceiptVO5 = new SalesSellReceiptVO("JHT-20170101-11515", 12, LocalDateTime.now(), LocalDateTime.now().plusDays(1), ReceiptState.PENDING, -1, "linyuchao", "", "", list, "2", 12, 12, 1882, null, 112223);
 
         try {
-            SalesSellblService salesSellblService= new SalesSellbl();
-
-            SalesSellReceiptVO ssr = salesSellblService.getNew();
-            System.out.println(ssr.getId());
-            salesSellReceiptVO.setId(ssr.getId());
-            salesSellReceiptVO.setCreateTime(ssr.getCreateTime());
-
-            salesSellblService.insert(salesSellReceiptVO);
-
-            System.out.println(salesSellblService.selectByMold(ssr).getId());
+//            SalesSellblService salesSellblService= new SalesSellbl();
+//
+//            SalesSellReceiptVO ssr = salesSellblService.getNew();
+//            ssr.setItems(list);
+//
+//            PromotionInfo promotionInfo = MyServiceFactory.getPromotionInfo();
+//            ArrayList<PromotionVO> promotionVOS =  promotionInfo.getMatch(ssr);
+//            System.out.println(promotionVOS.get(0));
 
 
-//            ArrayList<SalesSellReceiptVO> sss = sbl.search(new RespectiveReceiptSearchCondition());
-
-
+            PromotionblService<CombinePromotionVO> cpbl = MyblServiceFactory.getService(CombinePromotionblService.class);
+            cpbl.selectInEffect();
         } catch (Exception e) {
             e.printStackTrace();
         }
