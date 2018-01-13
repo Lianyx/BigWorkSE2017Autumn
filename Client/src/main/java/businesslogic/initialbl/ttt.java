38d7fb46e5f66b0d3ce4eb954fb639f9bdaeb00a
林鹏 <161250069@smail.nju.edu.cn>
2018-01-13 17:15:40 +0800

    modify

---
+++ Client/src/main/java/businesslogic/initialbl/ttt.java
@@ -0,0 +1,8 @@
+package businesslogic.initialbl;
+
+public class ttt {
+    public static void main(String[] args)throws Exception{
+        Initialbl initialbl = new Initialbl();
+        initialbl.initial("2018");
+    }
+}
