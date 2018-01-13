c4bd865077944ef9e67771ae12cec042fb31108a
DESKTOP-GM9RO08\dell <2860957268@qq.com>
2018-01-13 12:22:50 +0800

    update inventory

--- Client/src/main/java/businesslogic/goodsbl/goodsUpdate/GoodsSalesUpdate.java
+++ Client/src/main/java/businesslogic/goodsbl/goodsUpdate/GoodsSalesUpdate.java
@@ -27,63 +27,31 @@ public class GoodsSalesUpdate implements GoodsSalesUpdateInfo {
 
     @Override
     public void goodsUpdateSaleRet(List<ListGoodsItemVO> list) throws RemoteException {
-        List<ReceiptGoodsItemVO> warningList = new ArrayList<>();
 
         for (ListGoodsItemVO vo:list) {
             GoodsVO goodsVO = goodsbl.showDetail(vo.getGoodsId());
-
             int inventoryNum = goodsVO.getInventoryNum()-vo.getGoodsNum();
 
-            if(checkWarning(inventoryNum,goodsVO.getInventoryNum())){
-                ReceiptGoodsItemVO goodsItemVO = new ReceiptGoodsItemVO();
-                goodsItemVO.setGoodsId(goodsVO.getId());
-                goodsItemVO.setGoodsName(goodsVO.getGoodName());
-                goodsItemVO.setInventoryNum(inventoryNum);
-                goodsItemVO.setWarningNum(goodsItemVO.getWarningNum());
-                warningList.add(new ReceiptGoodsItemVO());
-            }
+
 
             goodsVO.setRecentSalePrice(vo.getPrice());
             goodsVO.setInventoryNum(inventoryNum);
             goodsbl.updateGoods(goodsVO);
         }
-
-        if(warningList.size() > 0){
-            InventoryWarningReceiptVO receiptVO = inventoryWarningReceiptbl.getNew();
-            receiptVO.setItems(warningList);
-            receiptVO.setOperatorId(UserInfomation.userid);
-            inventoryWarningReceiptbl.update(receiptVO);
-        }
-
     }
 
     @Override
     public void goodsUpdateSalesSel(List<ListGoodsItemVO> list) throws RemoteException {
-        List<ReceiptGoodsItemVO> warningList = new ArrayList<>();
+
         for (ListGoodsItemVO vo:list) {
             GoodsVO goodsVO = goodsbl.showDetail(vo.getGoodsId());
 
             int inventoryNum = goodsVO.getInventoryNum()-vo.getGoodsNum();
 
-            if(checkWarning(inventoryNum,goodsVO.getInventoryNum())){
-                ReceiptGoodsItemVO goodsItemVO = new ReceiptGoodsItemVO();
-                goodsItemVO.setGoodsId(goodsVO.getId());
-                goodsItemVO.setGoodsName(goodsVO.getGoodName());
-                goodsItemVO.setInventoryNum(inventoryNum);
-                goodsItemVO.setWarningNum(goodsItemVO.getWarningNum());
-                warningList.add(new ReceiptGoodsItemVO());
-            }
-
             goodsVO.setRecentSalePrice(vo.getPrice());
             goodsVO.setInventoryNum(inventoryNum);
             goodsbl.updateGoods(goodsVO);
 
-            if(warningList.size() > 0){
-                InventoryWarningReceiptVO receiptVO = inventoryWarningReceiptbl.getNew();
-                receiptVO.setItems(warningList);
-                receiptVO.setOperatorId(UserInfomation.userid);
-                inventoryWarningReceiptbl.update(receiptVO);
-            }
         }
     }
 
@@ -103,37 +71,21 @@ public class GoodsSalesUpdate implements GoodsSalesUpdateInfo {
 
     @Override
     public void goodsUpdateStorckRet(List<ListGoodsItemVO> list) throws RemoteException {
-        List<ReceiptGoodsItemVO> warningList = new ArrayList<>();
+
         for (ListGoodsItemVO vo:list) {
             GoodsVO goodsVO = goodsbl.showDetail(vo.getGoodsId());
 
             int inventoryNum = goodsVO.getInventoryNum()-vo.getGoodsNum();
 
-            if(checkWarning(inventoryNum,goodsVO.getInventoryNum())){
-                ReceiptGoodsItemVO goodsItemVO = new ReceiptGoodsItemVO();
-                goodsItemVO.setGoodsId(goodsVO.getId());
-                goodsItemVO.setGoodsName(goodsVO.getGoodName());
-                goodsItemVO.setInventoryNum(inventoryNum);
-                goodsItemVO.setWarningNum(goodsItemVO.getWarningNum());
-                warningList.add(new ReceiptGoodsItemVO());
-            }
 
             goodsVO.setRecentPurPrice(vo.getPrice());
             goodsVO.setInventoryNum(inventoryNum);
             goodsbl.updateGoods(goodsVO);
 
-            if(warningList.size() > 0){
-                InventoryWarningReceiptVO receiptVO = inventoryWarningReceiptbl.getNew();
-                receiptVO.setItems(warningList);
-                receiptVO.setOperatorId(UserInfomation.userid);
-                inventoryWarningReceiptbl.update(receiptVO);
-            }
         }
 
     }
 
 
-    private boolean checkWarning(int now,int inventoryNum){
-        return (now<inventoryNum)?true:false;
-    }
+
 }
